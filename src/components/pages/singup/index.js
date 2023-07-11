8aa069d61f63d417f338d9119ef276800a35f819
Kang GyeongWon <88028208+Kang-Gyeongwon@users.noreply.github.com>
2023-07-12 00:57:21 +0900

    feat: signup에 필요한 기본 html 작성

--- src/components/pages/singup/index.js
+++ src/components/pages/singup/index.js
@@ -1,5 +1,23 @@
 const SingUp = () => {
-  return <>SingUp</>;
+  return (
+    <>
+      <form>
+        <h1>회원정보 입력</h1>
+        <div>
+          <label>
+            <span>아이디</span>
+          </label>
+          <input type="text" />
+        </div>
+        <div>
+          <label>
+            <span>비밀번호</span>
+          </label>
+          <input type="password" />
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default SingUp;
