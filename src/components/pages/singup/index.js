584acf3a4a07d201a80beaa77f692d11bcdba26f
Kang GyeongWon <88028208+Kang-Gyeongwon@users.noreply.github.com>
2023-07-12 13:00:56 +0900

    feat: 회원가입 서버랑연결시키기

--- src/components/pages/singup/index.js
+++ src/components/pages/singup/index.js
@@ -1,4 +1,40 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const SingUp = () => {
+  const navigate = useNavigate();
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleValueUpdate = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch("http://3.38.191.164/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formValues),
+      });
+      if (response.ok) {
+        console.log("서버와의 연결이 성공적으로 이뤄졌습니다.");
+        navigate("/signin");
+      } else {
+        console.log("서봐와의 연결에 문제 발생");
+      }
+    } catch (error) {
+      console.log("서버와의 연결 중 오류 발생", error);
+    }
+  };
+
   return (
     <>
       <form>
@@ -7,14 +43,25 @@ const SingUp = () => {
           <label>
             <span>아이디</span>
           </label>
-          <input type="text" />
+          <input
+            type="text"
+            name="email"
+            value={formValues.email}
+            onChange={handleValueUpdate}
+          />
         </div>
         <div>
           <label>
             <span>비밀번호</span>
           </label>
-          <input type="password" />
+          <input
+            type="password"
+            name="password"
+            value={formValues.password}
+            onChange={handleValueUpdate}
+          />
         </div>
+        <button onClick={handleSubmit}>가입하기</button>
       </form>
     </>
   );
