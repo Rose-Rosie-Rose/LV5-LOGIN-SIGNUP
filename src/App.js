38044c341c3103b70d502b413c0bded4f3a395ed
Kang GyeongWon <88028208+Kang-Gyeongwon@users.noreply.github.com>
2023-07-13 15:05:16 +0900

    docs: 기본 코드 입력

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUpPage from "./pages/signup";
 import SignInPage from "./pages/signin";
+import HomePage from "./pages/home";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>homepage</div>} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/signin" element={<SignInPage />} />
       </Routes>
---
+++ src/components/signout/index.js
@@ -0,0 +1,5 @@
+const SignOut = () => {
+  return <>SignOut</>;
+};
+
+export default SignOut;
